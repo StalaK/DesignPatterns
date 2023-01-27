72822ab98613f58a658e8d01d092024f3db96d12
Josh Hughes <stalak@gmail.com>
2023-01-27 17:44:13 +0000

    Unsaved file

--- DecoratorPattern/Program.cs
+++ DecoratorPattern/Program.cs
@@ -11,7 +11,7 @@ Console.WriteLine($"Drink 1: {drink1.GetDescription()} - Cost: {drink1.Cost()}")
 IBeverage drink2 = new SteamedMilk(new Soy(new Whip(new Decaf())));
 Console.WriteLine($"Drink 2: {drink2.GetDescription()} - Cost: {drink2.Cost()}");
 
-// We can use extension methods instead
+// We can use extension methods to tidy this up and make it fluent
 var drink3 = new Espresso()
     .AddSoy()
     .AddMocha()
